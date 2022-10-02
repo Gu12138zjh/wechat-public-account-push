e3e2d248c91eb9dd2e108113cd569df05d4343f7
Gu12138zjh <114854489+Gu12138zjh@users.noreply.github.com>
2022-10-02 19:12:26 +0800

    Update and rename config/index.cjs to 配置/index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -8,56 +8,46 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx6545dc86f9b25fd4',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'a2b5eb6280bc837d9621315b6c34fa1b',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '甘肃',
+  CITY: '兰州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '笨蛋娜娜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oG38j55Q3YUWlsCuv4VwKDDlQxC0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-06',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '笨蛋娜娜', year: '2004', date: '10-06',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '笨蛋娜娜', year: '2004', date: '08-23',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '06-01',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '4oQ4rRvQfl3bOxQdYSBnxPZLUk9DJOlGsQnOZEjASzo',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '小乖',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oG38j52ANxpmDohwt9NZ_671XAd0',
     }
   ],
 
