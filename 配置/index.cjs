3861fbe48f9af14e2c0c9297099e2cefbc3de828
Gu12138zjh <114854489+Gu12138zjh@users.noreply.github.com>
2022-10-02 19:29:09 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oG38j55Q3YUWlsCuv4VwKDDlQxC0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '4oQ4rRvQfl3bOxQdYSBnxPZLUk9DJOlGsQnOZEjASzo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-06',
       festivals: [
